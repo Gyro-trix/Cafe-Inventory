36f03c6cfed902a04b8de6ac81d841a1211a914b
Matthew Garrett <m.garrett@hotmail.ca>
2023-11-26 15:48:14 -0330

    More experimentation

--- pantrypal.js
+++ pantrypal.js
@@ -1,6 +1,6 @@
 //Types of storage conatiners
 const typeid = ["fridge","fridgefzr","chestfzr","standfzr","groundcpb","wallcpb"];
-
+let taboneout = false;
 let temp = document.getElementById("tabone");
 temp.addEventListener("click",function (){slideOutDiv("tabone")});
 
@@ -13,5 +13,12 @@ function inventoryItem(name,num,loc,exp){
 }
 function slideOutDiv(id){
     let temp = document.getElementById(id);
-    temp.style.transform = 'translate3d(50px,0px,0px)';
+    if(taboneout === false){
+        temp.style.transform = 'translate3d(50px,0px,0px)';
+        taboneout = true;
+    } else {
+        temp.style.transform = 'translate3d(0px,0px,0px)';
+        taboneout = false;
+    }
+    
 }
